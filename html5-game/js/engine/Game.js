4cbe1f7fd2e3240e7d637508cdcb9d16d6fca8a0
dn <s66122519023@ssru.ac.th>
2026-02-15 23:12:34 +0700

    บันทึกข้อมูลFog of War (พื้นที่ที่สำรวจแล้ว)

--- html5-game/js/engine/Game.js
+++ html5-game/js/engine/Game.js
@@ -1259,7 +1259,8 @@ export class Game {
                 productionQueue: b.productionQueue.map(q => ({ ...q })),
                 productionProgress: b.productionProgress,
                 resourceTimer: b.resourceTimer
-            }))
+            })),
+            fogOfWar: this.fogOfWar ? Array.from(this.fogOfWar.visibility) : null
         };
 
         localStorage.setItem('rts_yutthakan_save', JSON.stringify(saveData));
@@ -1317,6 +1318,12 @@ export class Game {
             // Initialize Fog of War
             if (this.currentMap.fogOfWar !== false && this.settings.fogOfWarEnabled) {
                 this.fogOfWar = new FogOfWar(this.mapWidth, this.mapHeight, 32);
+                // Restore explored areas from save data
+                if (saveData.fogOfWar && saveData.fogOfWar.length === this.fogOfWar.visibility.length) {
+                    this.fogOfWar.visibility.set(saveData.fogOfWar);
+                    this.fogOfWar.visibilityChanged = true;
+                    this.fogOfWar.updateFogTexture();
+                }
             } else {
                 this.fogOfWar = null;
             }
